657ea7eeaf9ccedc167c82511c89724d397fbb6c
Felipe Laverdi <laverdifelipe@gmail.com>
2026-08-18 08:52:44 -0300

    Create Área do triângulo.py

---
+++ Python/Área do triângulo.py
@@ -0,0 +1,5 @@
+print("Área do triângulo")
+base = float(input("Digite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+area = (base * altura) / 2
+print(f"A área do triângulo é: {area}")
