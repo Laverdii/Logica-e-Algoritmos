4f15174832e9f62b0de0507979a9a4270eb84210
Felipe Laverdi <laverdifelipe@gmail.com>
2026-08-18 08:50:46 -0300

    Create Dobro e Triplo.py

---
+++ Python/Dobro e Triplo.py
@@ -0,0 +1,8 @@
+print("Dobro e Triplo")
+a = int(input("Digite um número para descobrir seu dobro e triplo: "))
+
+dobro = a * 2
+triplo = a * 3
+
+print(f"O dobro de {a} é: {dobro}\n")
+print (f"O triplo de {a} é: {triplo}")
