f633e166ef47b84adb377a10280e1d547a4b4368
Felipe Laverdi <laverdifelipe@gmail.com>
2026-08-18 08:54:23 -0300

    Create Identificador de sinal.py

---
+++ Python/Identificador de sinal.py
@@ -0,0 +1,9 @@
+print("Identificador de sinal\n")
+num1 = float(input("Digite um número para saber qual seu sinal: "))
+
+if (num1 > 0) :
+  print("Seu número é positivo")
+elif (num1 == 0) :
+  print("Seu número é zero")
+else :
+  print("Seu número é negativo")
