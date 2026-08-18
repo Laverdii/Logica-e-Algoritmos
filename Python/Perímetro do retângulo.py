a0b8cd4f5d057232efa92ebf6581778c9b0a8258
Felipe Laverdi <laverdifelipe@gmail.com>
2026-08-18 08:52:13 -0300

    Update Perímetro do retângulo.py

--- Python/Perímetro do retângulo.py
+++ Python/Perímetro do retângulo.py
@@ -1,4 +1,4 @@
-print("print("Perímetro do retângulo")
+print("Perímetro do retângulo")
 lado1 = float(input("Digite o comprimento do retângulo: "))
 lado2 = float(input("Digite a altura do retângulo: "))
 perimetro = (lado1 * 2) + (lado2 * 2)
