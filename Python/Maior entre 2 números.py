bbda124037e89f51c5a5beee3233653961ac3e7b
Felipe Laverdi <laverdifelipe@gmail.com>
2026-08-18 08:53:10 -0300

    Add script to find the greater of two numbers

---
+++ Python/Maior entre 2 números.py
@@ -0,0 +1,8 @@
+print("Maior entre 2 números")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+if num1 > num2 :
+  print(f"O número {num1}, é maior que {num2}")
+else :
+  print(f"O número {num2}, é maior que {num1}")
