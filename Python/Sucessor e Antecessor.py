ee8ade85f861157926ad9cd9f8b950be25b06981
Felipe Laverdi <laverdifelipe@gmail.com>
2026-08-18 08:51:20 -0300

    Create Sucessor e Antecessor.py

---
+++ Python/Sucessor e Antecessor.py
@@ -0,0 +1,7 @@
+print("Sucessor/Antecessor\n")
+num = int(input("Digite um número para saber seu sucessor e antecessor: "))
+
+sucessor = num + 1
+antecessor = num - 1
+
+print(f"O sucessor de {num} é: {sucessor}\nO antecessor de {num} é: {antecessor}")
