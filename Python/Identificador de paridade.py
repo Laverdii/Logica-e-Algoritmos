9002f35adc759878d8072d1e8c7b6cddbfe7154b
Felipe Laverdi <laverdifelipe@gmail.com>
2026-08-18 08:54:01 -0300

    Create Identificador de paridade.py

---
+++ Python/Identificador de paridade.py
@@ -0,0 +1,7 @@
+print("Identificador de paridade\n")
+num1 = float(input("Digite um número para descobrir sua paridade: "))
+
+if (num1 % 2 == 0) :
+    print(f"O número {num1} é par")
+else :
+    print(f"O número ", num1, " é ímpar")
