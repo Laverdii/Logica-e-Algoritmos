e9255af70ec58223df5fd0f9c7fe4103c3a19e3c
Felipe Laverdi <laverdifelipe@gmail.com>
2026-09-01 08:27:09 -0300

    Create vprintClassificação de triângulo.py

---
+++ Python/vprintClassificação de triângulo.py
@@ -0,0 +1,11 @@
+print("Classificação de triângulo")
+lado1 = float(input("Digite o primeiro lado do triângulo: "))
+lado2 = float(input("Digite o segundo lado do triângulo: "))
+lado3 = float(input("Digite o terceiro lado do triângulo: "))
+
+if ((lado1 == lado2) and == lado3) :
+  print("Você tem um triângulo equilátero")
+elif ((lado1 != lado2) and != lado3) :
+  print("Você tem um triângulo escaleno")
+else :
+  print("Você tem um triângulo isósceles")
