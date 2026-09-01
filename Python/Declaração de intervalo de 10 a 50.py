c178dce2b557c8af611dbaa0290ff809c543b251
Felipe Laverdi <laverdifelipe@gmail.com>
2026-09-01 08:24:43 -0300

    Add range check for numbers between 10 and 50

---
+++ Python/Declaração de intervalo de 10 a 50.py
@@ -0,0 +1,6 @@
+print("Declaração de intervalo de 10 a 50")
+num = int(input("Digite um número para verificar se ele está entre 10 e 50: "))
+if (num >= 10 and num <= 50) :
+  print("Está entre 10 e 50")
+else :
+  print("Seu número não está entre 10 e 50.")
